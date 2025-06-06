a03f1e55917e486284b8e50d0c9ce245c847bb4b
Lpavithran210 <lpavi210@gmail.com>
2025-06-06 15:58:42 +0530

    backend url changed

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import courseRoute from './routes/courseRoute.js'
 
 dotenv.config()
 const app = express()
-app.use(cors())
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true
+};
+app.use(cors(corsOptions))
 app.use(express.json({ limit: '10mb' }));
 app.set("view engine", "ejs")
 
