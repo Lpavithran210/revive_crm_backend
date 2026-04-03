13c6ffa96562d690847b175046f4977bfbd08fc5
Pavithran <55428571+Lpavithran210@users.noreply.github.com>
2026-04-03 16:05:19 +0530

    followup routes fix

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createStudent, getDueFollowups, getEnquiries, lockReminder, markReminderSent, updateStudent, uploadStudents } from '../controllers/studentController.js'
+import { createStudent, getEnquiries, updateStudent, uploadStudents } from '../controllers/studentController.js'
 import { authorizeRoles } from '../middlewares/authorizeRoles.js'
 import protect from '../middlewares/authMiddleware.js'
 
@@ -10,12 +10,6 @@ router.post('/upload-students', protect, authorizeRoles('admin'), uploadStudents
 router.post('/automation/create-student', createStudent)
 
 router.post('/create-student', protect, authorizeRoles('admin'), createStudent)
-
-router.get('/due-followups', getDueFollowups);
-
-router.put('/lock-reminder/:studentId/:followupId', lockReminder);
-
-router.put('/mark-reminder-sent/:studentId/:followupId', markReminderSent);
 
 router.get('/enquiries', protect, getEnquiries)
 
