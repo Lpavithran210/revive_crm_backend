26cf9767a53bee3c07169c3785add4561f5c8734
Pavithran <55428571+Lpavithran210@users.noreply.github.com>
2026-04-06 19:27:30 +0530

    socket emitt

--- followupReminderCron.js
+++ followupReminderCron.js
@@ -12,16 +12,16 @@ cron.schedule('* * * * *', async () => {
     console.log("Searching between:", lowerBound, upperBound);
     try {
         const students = await StudentModel.find({
-        history: {
-            $elemMatch: {
-            follow_up_date: {
-                $gte: lowerBound,
-                $lt: upperBound
-            },
-            status: "Follow up",
-            reminder_sent: { $ne: true }
+            history: {
+                $elemMatch: {
+                    follow_up_date: {
+                        $gte: lowerBound,
+                        $lt: upperBound
+                    },
+                    status: "Follow up",
+                    reminder_sent: { $ne: true }
+                }
             }
-        }
         });
         console.log("Students found:", students.length);
 
@@ -29,9 +29,10 @@ cron.schedule('* * * * *', async () => {
             let updated = false;
 
             for (const followup of student.history) {
+                const followupTime = new Date(followup.follow_up_date);
+
                 if (!followup.follow_up_date) continue;
                 if (followup.reminder_sent) continue;
-                const followupTime = new Date(followup.follow_up_date);
 
                 if (
                     followup.status === 'Follow up' &&
@@ -44,18 +45,18 @@ cron.schedule('* * * * *', async () => {
                     if (!attenderId) continue;
 
                     console.log("EMITTING TO", attenderId);
+
                     io.to(attenderId.toString()).emit("followupReminder", {
                         name: student.name,
                         phone: student.phone,
                         course: student.course,
                         followupTime: followup.follow_up_date,
                         note: followup.note,
-                        attenderId: attenderId,
+                        attenderId,
                         attender: attenderName
                     });
 
                     followup.reminder_sent = true;
-                    updated = true;
                 }
             }
             if (updated) {
